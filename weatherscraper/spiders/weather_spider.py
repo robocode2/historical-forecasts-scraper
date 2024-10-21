25dd29a3b904c8404ed2079cb066a11d3466cfef
robocode2 <rabie.abbas@code.berlin>
2024-10-21 16:30:09 +0200

    * Adjusted code after website changed (now using XPath instead of CSS selectors) *  Improved scraping the first element

--- weatherscraper/spiders/weather_spider.py
+++ weatherscraper/spiders/weather_spider.py
@@ -21,41 +21,55 @@ class TheWeatherChannelSpider(scrapy.Spider):
     def parse(self, response):
         city = response.meta.get('city')
         country = response.meta.get('country')
-        state = response.meta.get('state')
-        if state == '':
-            state = None
+        state = response.meta.get('state') or ''
         current_date = datetime.now(timezone.utc)
-
+        
 
         for i in range(15):
-            day_selector = f'//*[@id="detailIndex{i}"]/div/div[2]/ul/li[1]/div/span[2]'
-            humidity = response.xpath(day_selector + '/text()').get()
+            day_selector = f'//*[@id="detailIndex{i}"]'
+
+            # Extract humidity
+            humidity = response.xpath(day_selector + '/div/div[2]/ul/li[1]/div/span[2]/text()').get()
             if humidity:
                 humidity = humidity.replace('%', '').strip()
-
-            day = response.xpath(f'//*[@id="detailIndex{i}"]')
 
             item = DayForecastItem()
             item['country'] = country
             item['state'] = state
             item['city'] = city
-            item['date'] = datetime.now().strftime('%Y-%m-%d')
-            item['day'] = day.css('h2.DetailsSummary--daypartName--kbngc::text').get()
-            item['weather_condition'] = day.css('div.DetailsSummary--condition--2JmHb span::text').get()
-            # Update for precipitation chance extraction
-            precip_selector = day.css('div.DailyContent--precipIconBlock--LoWxx span.DailyContent--value--Xgh8M::text').get()
-            if precip_selector:
-                item['precipitation_chance'] = precip_selector.replace('%', '').strip()
+     
+
+            if i == 0:
+                item['precipitation_chance'] = response.xpath('//*[@id="detailIndex0"]/div/div[1]/div/div[3]/div[1]/span/text()').get()
+                item['weather_condition'] = response.xpath('//*[@id="detailIndex0"]/div/div[1]/div/div[2]/svg/title/text()').get()
             else:
-                item['precipitation_chance'] = None            
+                item['precipitation_chance'] = (
+                response.xpath(day_selector + '/div[@class="DailyContent--precipIconBlock--LoWxx"]/span[@class="DailyContent--value--Xgh8M"]/text()').get() or
+                response.xpath(day_selector + '/summary/div/div/div[3]/span[@data-testid="PercentageValue"]/text()').get()
+            )
+                item['weather_condition'] = response.xpath(day_selector + '/div[@class="DetailsSummary--condition--2JmHb"]/span/text()').get() or \
+                response.xpath(day_selector + '/summary/div/div/div[1]/svg/title/text()').get()
+
+                
+            if item['precipitation_chance']:
+                item['precipitation_chance'] = item['precipitation_chance'].replace('%', '').strip()
+            else:
+                item['precipitation_chance'] = None
+            
             item['precipitation_amount'] = None
-            item['wind_speed'] = day.css('span[data-testid="Wind"] span:nth-child(2)::text').extract_first()
+
+            item['wind_speed'] = response.xpath(day_selector + '/div/div[1]/div/div[3]/div[2]/span/span[2]/text()').get() or \
+                response.xpath(day_selector + '/div/div[1]/div/div[3]/div[2]/span/span[2]/text()').get()
+
             item['humidity'] = humidity
             item['source'] = 'TheWeatherChannel'
-            temp_high = day.css('span.DetailsSummary--highTempValue--3PjlX::text').get()
+            
+            temp_high = response.xpath(day_selector + '//*[contains(@class, "DetailsSummary--highTempValue")]/text()').get()
             item['temp_high'] = temp_high if temp_high != "--" else None
-            temp_low = day.css('span.DetailsSummary--lowTempValue--2tesQ::text').get()
+
+            temp_low = response.xpath(day_selector + '//*[contains(@class, "DetailsSummary--lowTempValue")]/text()').get()
             item['temp_low'] = temp_low if temp_low != "--" else None
-            item['date']= current_date
-            item['day']= (current_date + timedelta(days=i))
+            
+            item['date'] = current_date  
+            item['day'] = current_date + timedelta(days=i) 
             yield item
