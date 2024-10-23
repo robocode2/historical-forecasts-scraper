25d4cb7f6703183c22705df6b97316a06c8c5b94
robocode2 <rabie.abbas@code.berlin>
2024-10-23 22:03:58 +0200

    combat 429 error by introducing concurrency limit

--- weatherscraper/settings.py
+++ weatherscraper/settings.py
@@ -27,40 +27,26 @@ SCRAPEOPS_PROXY_ENABLED = True
 
 SCRAPEOPS_NUM_RESULTS = 50
 
+CONCURRENT_REQUESTS = 1 
+DOWNLOAD_DELAY = 2
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "weatherscraper (+http://www.yourdomain.com)"
-
-
-
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 10
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
 #    "weatherscraper.middlewares.WeatherscraperSpiderMiddleware": 543,
 #}
+
+""" SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = which("chromedriver")
+SELENIUM_DRIVER_ARGUMENTS = [] 
+ """
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
@@ -71,20 +57,8 @@ DOWNLOADER_MIDDLEWARES = {
     'weatherscraper.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware': 400,
 }
 
-# Other Scrapy settings
 ROBOTSTXT_OBEY = False
 
-
-""" SELENIUM_DRIVER_NAME = 'chrome'
-SELENIUM_DRIVER_EXECUTABLE_PATH = which("chromedriver")
-SELENIUM_DRIVER_ARGUMENTS = []  """
-
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
 
 EXTENSIONS = {
         'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
@@ -98,18 +72,7 @@ ITEM_PIPELINES = {
 }
 DATABASE_URL = os.getenv('PROD_DATABASE_URL')
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -123,5 +86,6 @@ DATABASE_URL = os.getenv('PROD_DATABASE_URL')
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
 
 
